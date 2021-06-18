a920b73a4383367cb20cdd50f3092a39d4770fed
Filippo Franchini <filippo.franchini@outlook.com>
2021-06-18 15:06:09 +0200

    Added selection function for STCS dummy data

--- R/helpers.R
+++ R/helpers.R
@@ -19,3 +19,78 @@ gfr_calc <- function(scr, age, gender){
   return(eGFR)
 
 }
+
+
+stcs_select <- function(datastring, eGFR.limit){
+
+  data <- read.xls(datastring, header = T, sheet = 1)
+
+  data <- data[!is.na(data$creatinin),] #removing NA creatinin NA rows
+
+  # Date formatting
+  data$tpxdate <- as.Date(data$tpxdate, "%Y-%m-%d")
+  data$birthday <- as.Date(data$birthday, "%Y-%m-%d")
+  data$creatinindate <- as.Date(data$creatinindate, "%Y-%m-%d")
+
+  # Removing useless columns
+  data <- data[,c(3:7,11:12)]
+
+  # Calculating age @ creatinin and eGFR
+
+  data$creatininAge <- as.vector(difftime(time2 = data$birthday, time1 = data$creatinindate))/365
+
+  data$sex <- as.factor(data$sex)
+  levels(data$sex) <- c("female", "male")
+
+  data$creatinin_c <- data$creatinin/10^6*113.12*10^3/10 #creatinin in mg/dl
+
+  egfrs <- c()
+
+  for(i in 1:length(data[,1])){
+
+    egfrs[i] <- gfr_calc(scr = data$creatinin_c[i],
+                         age = data$creatininAge[i],
+                         gender = data$sex[i])
+
+  }
+
+  data$egfr <- egfrs
+
+  # Calculating % change from baseline > 90 mL/min/1.73m2
+
+  ids <- unique(data$patid)
+
+  change <- list()
+
+  for(i in 1:length(ids)){
+
+    sub.data <- subset(data, patid == ids[i])
+
+    if(sub.data$egfr[sub.data$assperiod == 0] >= eGFR.limit){
+
+      sub.data$change <- abs(sub.data$egfr - sub.data$egfr[sub.data$assperiod == 0])/sub.data$egfr[sub.data$assperiod == 0]*100
+
+    } else {
+
+      sub.data$change <- NA
+
+    }
+
+    change[[i]] <- sub.data
+
+  }
+
+  data.final <- do.call(change,what = rbind)
+  data.final <- na.omit(data.final) # remove patients with baseline < 90
+  data.final <- data.final[!data.final$change == 0,] # remove baselines
+
+  data.final <- data.final[data.final$change >= 25,] # take only data with change >= 25%
+
+  sum.tab1 <- group_by(data.final, organ, patid) %>% summarize(n = length(change))
+
+  sum.tab2 <- group_by(sum.tab, organ) %>% summarize(npat = length(patid))
+
+  return(list(tab1 = data.frame(sum.tab1), tab2 = data.frame(sum.tab2)))
+
+
+}
