d42cc297423a8f00f8eb1567b55bb64132112943
Filippo Franchini <filippo.franchini@outlook.com>
2021-06-18 15:21:10 +0200

    Minor edits

--- scripts/stcs_script.R
+++ scripts/stcs_script.R
@@ -1,6 +1,17 @@
-# Loading excel data----
-
 library(gdata)
 library(dplyr)
 
 stcs_select(datastring = "PGX_example_17JUN21.xlsx", eGFR.limit = 90)
+
+
+
+## Do not run ##
+#Output example using the dummy data
+
+#$tab1
+#organ    patid n
+#1  Lung 20004000 4
+
+#$tab2
+#organ npat
+#1  Lung    1
