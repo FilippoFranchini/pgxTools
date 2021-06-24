cae1df16fc7cc5cb2acb8617d40234d011365340
Filippo Franchini <filippo.franchini@outlook.com>
2021-06-24 09:36:31 +0200

    Updated script

--- scripts/stcs_script.R
+++ scripts/stcs_script.R
@@ -1,19 +1,22 @@
 library(gdata)
 library(dplyr)
 
-stcs_select(datastring = "PGX_example_17JUN21.xlsx", l.cs = 20, l.ct = 10)
-
 stcs_select(datastring = "PGX_example_17JUN21.xlsx", l.cs = 25, l.ct = 15)
 
-
-
 ## Do not run ##
-#Output example using the dummy data
 
 #$tab1
-#organ    patid n
-#1  Lung 20004000 4
+#organ    patid n        info         E         SE        R2
+#1  Lung 20004000 4 36 48 60 72 0.4141099 0.06424958 0.9540677
 
 #$tab2
 #organ npat
 #1  Lung    1
+
+#$tab3
+#organ    patid n
+#1 Liver 10003000 6
+
+#$tab4
+#organ n
+#1 Liver 1
